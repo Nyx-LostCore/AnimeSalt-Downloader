39a07cd61dd547288e90cd2f3f7e20687f30c887
×͜× 「 ʟᴏꜱᴛ ᴇxɪꜱᴛᴇɴᴄᴇ 」 ×͜× <thebochan9@gmail.com>
2026-07-24 19:03:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ API_HASH = os.getenv("API_HASH", "your_api_hash_here")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token_here")
 
 # AnimeSalt API Base Endpoint
-ANIMESALT_API_URL = os.getenv("ANIMESALT_API_URL", "https://animesalt-api-endpoint-url.com")
+ANIMESALT_API_URL = os.getenv("ANIMESALT_API_URL", "https://animesalt-api-nine.vercel.app/")
 
 # Custom Rename & Upload Formatting
 RENAME_FORMAT = os.getenv("RENAME_FORMAT", "[S{season}-E{episode}] {title} [{quality}] [{audio}] @Anime_Rage_official.mp4")
